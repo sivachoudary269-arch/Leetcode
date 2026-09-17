926f97674e58dfb069ae991cc5315e010deaaf02
Siva mutyala <sivamutyala7203@gmail.com>
2026-09-17 18:39:07 +0530

    Time: 0 ms (100.00%) | Memory: 100.5 MB (6.78%) - LeetSync

---
+++ 932-monotonic-array/monotonic-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        bool incr=true;
+        bool decr=true;
+       for(int i=1;i<nums.size();i++){
+          if(nums[i]<nums[i-1]){
+            incr=false;
+          }
+          if(nums[i]>nums[i-1]){
+            decr=false;
+          }
+       }
+       return incr||decr;
+    }
+};
